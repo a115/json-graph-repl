bc112171ed307e3bf61a41332b7143e66697f338
Jordan Dimov <jdimov@a115.bg>
2017-07-19 15:11:07 +0100

    Fix for jgrepl.py

--- jgrepl/jgrepl.py
+++ jgrepl/jgrepl.py
@@ -4,10 +4,13 @@ import argparse
 
 from jgrepl.repl import JSONRepl
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath", help="path to a JSON graph to load")
     args = parser.parse_args()
 
     app = JSONRepl(filepath=args.filepath)
     app.cmdloop()
+
+if __name__ == '__main__':
+    main()
